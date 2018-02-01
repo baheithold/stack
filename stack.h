510b13fcbd6e64e804b0970ca0b7240d4a97af6a
Brett Heithold <baheithold@crimson.ua.edu>
2018-02-01 03:15:58 -0600

    Created the stack.h interface file.

---
+++ stack.h
@@ -0,0 +1,23 @@
+/*
+ *  File:   stack.h
+ *  Author: Brett Heithold
+ *  Description: This is the interface for the stack class.
+ */
+
+#ifndef __STACK_INCLUDED__
+#define __STACK_INCLUDED__
+
+#include <stdio.h>
+
+typedef struct stack STACK;
+
+extern STACK *newSTACK(void (*d)(void *, FILE *), void (*f)(void *));
+extern void push(STACK *items, void *value);
+extern void *pop(STACK *items);
+extern void *peekSTACK(STACK *items);
+extern int sizeSTACK(STACK *items);
+extern void displaySTACK(STACK *items, FILE *);
+extern void displaySTACKdebug(STACK *items, FILE *);
+extern void freeSTACK(STACK *items);
+
+#endif // !__STACK_INCLUDED__
